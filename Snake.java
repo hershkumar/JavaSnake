05e80d143b3a4e5738a4d191dde1c6f6796bf153
hershkumar <33847994+hershkumar@users.noreply.github.com>
2017-12-15 09:18:22 -0500

    Update Snake.java

--- Snake.java
+++ Snake.java
@@ -1,4 +1,59 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Snake {
-
+import javax.swing.*;
+public class Snake extends JFrame implements ActionListener{
+	private int snakeLength = 1;
+	private static boolean playing = false;
+	//makeScreen()
+	private static void makeScreen() {
+		JFrame screen = new JFrame();
+		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		screen.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		screen.getContentPane().setBackground(Color.cyan);
+		screen.setVisible(true);
+		screen.setResizable(false);
+		Graphics g = screen.getGraphics();
+		g.setColor(Color.red);
+		playing = true;	
+		drawSnake(g);
+		
+	}
+	@Override
+	public void paintComponents(Graphics h) {
+		super.paintComponents(h);
+		drawSnake(h);
+		repaint();
+	}
+	
+	
+	
+	
+	//updateScreen()
+	//drawSnake()
+	private static void drawSnake(Graphics g) {
+		if (playing) {
+			g.fillRect(100,100,100,100);
+			
+			
+		}
+	}
+	//move()
+	//checkLocation()
+	//increaseLength
+	//gameOver()
+	//checkSelfCollision()
+	
+	
+	public static void main(String[] args) {
+		makeScreen();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
