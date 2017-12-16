8960a2c411df8f228e0650ca51a0e17a2d78545d
hershkumar <33847994+hershkumar@users.noreply.github.com>
2017-12-16 12:15:11 -0500

    Update Snake.java

--- Snake.java
+++ Snake.java
@@ -1,59 +1,28 @@
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+package net.mrpaul.md110.psExtra;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+public class Snake extends JFrame{
 
-import javax.swing.*;
-public class Snake extends JFrame implements ActionListener{
-	private int snakeLength = 1;
-	private static boolean playing = false;
-	//makeScreen()
-	private static void makeScreen() {
-		JFrame screen = new JFrame();
-		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		screen.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		screen.getContentPane().setBackground(Color.cyan);
-		screen.setVisible(true);
-		screen.setResizable(false);
-		Graphics g = screen.getGraphics();
-		g.setColor(Color.red);
-		playing = true;	
-		drawSnake(g);
-		
+	public Snake(){
+
+		add(new BoardMaker());
+
+		setResizable(true);
+		pack();
+		setTitle("Snake");
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	@Override
-	public void paintComponents(Graphics h) {
-		super.paintComponents(h);
-		drawSnake(h);
-		repaint();
-	}
-	
-	
-	
-	
-	//updateScreen()
-	//drawSnake()
-	private static void drawSnake(Graphics g) {
-		if (playing) {
-			g.fillRect(100,100,100,100);
-			
-			
-		}
-	}
-	//move()
-	//checkLocation()
-	//increaseLength
-	//gameOver()
-	//checkSelfCollision()
-	
-	
+
 	public static void main(String[] args) {
-		makeScreen();
+
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {                
+				JFrame ex = new Snake();
+				ex.setVisible(true);                
+			}
+		}
+				);
 	}
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	
 }
